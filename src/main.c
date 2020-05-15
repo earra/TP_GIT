2b4e3072dc7dd4ff82a485594cb95168eeb3efee
earra <erwan.arrault@gmail.com>
2020-05-15 12:25:16 +0200

    first commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/source1.h"
+#include "../include/source2.h"
+
+int main(int argc, char const *argv[] ) {
+  int a = 10, b = 5;
+
+  printf("%d + %d = %d \n",a, b, somme(a, b) );
+  printf("%d - %d = %d \n",a, b, soustraction(a, b));
+  printf("%d - %d = %d \n",a, b, multiplication(a, b));
+return 0;
+}
---
+++ src/source1.c
@@ -0,0 +1,10 @@
+#include "../include/source1.h"
+
+int somme(int a, int b){
+  return a + b;
+}
+int multiplication(int a, int b){
+  return a * b;
+}
+
+
